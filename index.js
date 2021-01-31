060d6316cd386ec058d0b054fb627b8a3f25c7b7
Pontus Olsson <pontus.olsson86@gmail.com>
2021-01-31 19:48:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function run(){
-  window.location = "http://192.168.10.145:8443";
+  window.location = "https://192.168.10.145:8443";
 }
 //Här lägger vi in att run ska köras när dokumentet är laddat.
 document.addEventListener('DOMContentLoaded', run);
